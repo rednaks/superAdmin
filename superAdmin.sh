9f2bedcb858062ffb84330cd9e3059205ef10243
Alexandre BM <s@rednaks.tn>
2013-05-16 21:37:43 +0100

    manipulation du choix

--- superAdmin.sh
+++ superAdmin.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+
+
+clear;
 echo "[1] Gestion d'utilisateurs"
 echo "[2] Gestion des tâches planifiés"
 echo "[3] Gesiton des Logs"
 echo "[4] Gestion du réseaux"
 echo "[5] Quitter"
+
+echo "Faites votre choix : "
+read choix
+
+case $choix in
+  1) echo -e "Gestion d'utilisateurs...";;
+  2) echo -e "Gestion des tâches ...";;
+  3) echo -e "La journalisation ...";;
+  4) echo -e "Gestion du réseaux ...";;
+  *) echo -e "Ce choix n'existe pas !";;
+esac
