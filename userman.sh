251e39ee22ac3d4be43879f7c4a7673c249d78a6
Alexandre BM <s@rednaks.tn>
2013-05-16 23:59:38 +0100

    Ajout d'une fonction pour la création d'utilisateur

--- userman.sh
+++ userman.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+createUser(){
+  clear;
+  echo "** Création d'utilisateur";
+  while true
+  do
+    echo "Entrez le nom d'utilisateur :";
+    read uname;
+    test -n "$uname" && break
+  done
+  echo "The user name is : $uname";
+  echo "Entrez le mot de passe associé :";
+  read -s password;
+  echo "Voulez vous préciser les groupes ? [O/N]";
+  read yn
+  case $yn in
+    [Oo]) echo "Lister les groupes";;
+    [Nn]) echo "Continuer";;
+       *) echo "Ce choix n'existe pas";;
+  esac
+}
+
 clear;
 echo "[1] Créer un utilisateur"
 echo "[2] Supprimer des utilisateurs"
@@ -11,7 +32,8 @@ echo "[6] Quitter"
 read choix
 
 case $choix in
-  1) echo -e "Création d'un utilisateur ...";;
+  1) echo -e "Création d'un utilisateur ...";
+    createUser;;
   2) echo -e "Supression des utilisateurs ...";;
   3) echo -e "List des utilisateurs ...";;
   4) echo -e "Gestion des mots de passes...";;
@@ -19,3 +41,7 @@ case $choix in
   6) echo -e "Quit";;
   *) echo -e "Ce choix n'existe pas...";;
 esac
+
+
+
+
